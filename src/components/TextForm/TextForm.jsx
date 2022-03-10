c296394f0aff2a78780ab7bb79bf9e3172c52896
Hamza Aamir <hamzaamirm2001@gmail.com>
2022-03-10 22:19:09 +0500

    added more functionalities

--- src/components/TextForm/TextForm.jsx
+++ src/components/TextForm/TextForm.jsx
@@ -13,6 +13,10 @@ const TextForm = () => {
         setText(newText);
     }
 
+    const clearText = () => {
+        setText('');
+    }
+
     const gettingInput = (event) => {
         setText(event.target.value);
     }
@@ -27,6 +31,22 @@ const TextForm = () => {
         <div>
             <button onClick={upperCase}>Convert to Upper Case</button>
             <button onClick={lowerCase}>Convert to Lower Case</button>
+            <button onClick={clearText}>Clear Text</button>
+        </div>
+        <div>
+            <h3>Text Summary</h3>
+        </div>
+        <div>
+            Number of Words: {text.split(" ").length}
+        </div>
+        <div>
+            Number of Characters: {text.length}
+        </div>
+        <div>
+            {text.split(" ").length * 0.008} minutes read.
+        </div>
+        <div>
+            {text}
         </div>
     </div>
   )
